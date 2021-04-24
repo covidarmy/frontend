882e75478931a456269028bac4d6562455326b93
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2021-04-24 19:27:05 +0530

    minor improvements

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const { MONGODB_URI } = process.env
 
-if (!MONGODB_URI && false) {
+if (!MONGODB_URI) {
   throw new Error(
     "Please define the MONGODB_URI environment variable inside .env.local"
   )
@@ -37,11 +37,9 @@ const connectToDatabase = async () => {
       useCreateIndex: true,
     }
 
-    cached.promise = mongoose
-      .connect(MONGODB_URI || "mongodb://localhost:27017/covid_app", opts)
-      .then((mongoose) => {
-        return mongoose
-      })
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose
+    })
   }
   cached.conn = await cached.promise
   return cached.conn
