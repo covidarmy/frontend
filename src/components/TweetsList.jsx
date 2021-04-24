50d59ceb7ade79fda1c70b72def4b67bb8675c59
Naveen <thenvnsahu@gmail.com>
2021-04-24 23:19:51 +0530

    added buttons to upvote/downvote and copy link

--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router"
 import * as React from "react"
 import { Tweet } from "react-static-tweets"
+import {
+  ThumbUpIcon,
+  ThumbDownIcon,
+  DuplicateIcon,
+} from "@heroicons/react/outline"
 
 /**
  * @type {React.NamedExoticComponent}
@@ -18,9 +23,35 @@ const TweetsList = React.memo(({ data }) => {
         return (
           <div
             key={tweetId}
-            className="w-full flex flex-col items-center justify-center space-y-4 my-2"
+            className="w-full flex flex-col items-center justify-center space-y-4 my-8 px-2"
           >
             <Tweet id={tweetId} />
+            <div className="w-full sm:w-1/2 lg:w-2/5 xl:w-1/3 px-4 sm:px-0 flex justify-between">
+              <div className="flex justify-between space-x-8">
+                <div className="text-green-500 text-xs text-center hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <ThumbUpIcon className="w-8 sm" />
+                    <p className="text-sm">{voteCount > 0 ? voteCount : 0}</p>
+                  </div>
+                  <p className="text-gray-400">Working</p>
+                </div>
+
+                <div className="text-red-500 text-xs text-center hover:cursor-pointer">
+                  <div className="flex items-center">
+                    <ThumbDownIcon className="w-8" />
+                    <p className="text-sm">{voteCount < 0 ? voteCount : 0}</p>
+                  </div>
+                  <p className="text-gray-400">Not Working</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-gray-500 text-xs text-center hover:cursor-pointer">
+                  <DuplicateIcon className="w-8" />
+                  <p className="text-gray-400 text-xs">Copy</p>
+                </div>
+              </div>
+            </div>
+            <hr className="w-1/6 h-0.5 border-none bg-gray-300" />
           </div>
         )
       })
