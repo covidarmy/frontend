c67f81bb70bf74117f0d26acac5ac65de40139b0
Arnav Gosain <arnav@arnavgosain.com>
2021-04-26 14:16:51 +0530

    Update scrape-tweets script
    Signed-off-by: Arnav Gosain <arnav@arnavgosain.com>

--- scripts/scrape-tweets.js
+++ scripts/scrape-tweets.js
@@ -1,26 +1,8 @@
 require("dotenv").config()
 
 const { store } = require("../src/lib/firebase-admin")
-const { getTweets } = require("../src/lib/scrape")
-
-const filterAccounts = [
-  "IndiaToday",
-  "LiveLawIndia",
-  "ANI",
-  "PTI_NEWS",
-  "TOIMumbai",
-  "BDUTT",
-  "VtvGujarati",
-]
+const { scrape } = require("../src/lib/scrape")
 
 ;(async () => {
-  /** @type {import("../src/types").Cities} */
-  const cities = (await store.doc("main/cities").get()).data()
-  const resources = (await store.doc("main/resources").get()).data()
-
-  await getTweets(
-    process.env.CITY ? { [process.env.CITY]: true } : cities,
-    resources,
-    filterAccounts
-  )
+  await scrape({})
 })()
