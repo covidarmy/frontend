4732ecf0e5d30e9a37458a8e679871eae7061ee2
Arnav Gosain <arnav@arnavgosain.com>
2021-04-26 14:27:34 +0530

    Use router to push to /delhi (#9)

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ let config = {
   future: {
     webpack5: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/delhi",
-      },
-    ]
-  },
   images: {
     domains: ["pbs.twimg.com", "abs.twimg.com"],
   },
