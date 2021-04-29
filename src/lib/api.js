7b8d909c84e6a1779d075a7857f0e4b2db42ad9e
Apoorv <apoorvsingal0@gmail.com>
2021-04-29 21:58:56 +0530

    change api url to the new one

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch"
 
-const BASE_URL = "https://covidarmy-backend.apoorvsingal.repl.co/api/tweets"
+const BASE_URL = "http://64.225.84.16:4000/api/tweets"
 
 export const fetchTweets = ({ location, resource, limit, offset }) => {
   let url = BASE_URL
