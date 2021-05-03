4cffca9ab544f91a3d85a7eb90ef5d2670233af4
kanishkaverma <kanisverma@gmail.com>
2021-05-04 04:20:48 +0530

    Change navbar layout on mobile Fixes #41

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,33 @@ import { Disclosure } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 import Link from "next/link"
 import Logo from "../assets/Logo.svg"
+import { HiOutlineShare } from "react-icons/hi"
+import * as React from "react"
 
 export default function Navbar({ lastUpdated = 6 }) {
+  const [canShare, setCanShare] = React.useState(true)
+
+  React.useEffect(() => {
+    if (!navigator.share) setCanShare(false)
+  }, [])
+
   return (
     <Disclosure as="nav" className="shadow-lg bg-white w-full">
       {({ open }) => (
         <>
           <div className="flex items-center justify-between w-full h-16 px-4 lg:px-20">
-            <div className="lg:flex lg:ml-8 mr-20 sm:ml-10">
+            <div className="-mr-2 flex md:hidden">
+              {/* Mobile menu button */}
+              <Disclosure.Button className=" inline-flex items-center justify-center p-2 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-white">
+                <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <XIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </div>
+            <div className="flex flex-col lg:flex-row items-center justify-center lg:items-end lg:ml-8 sm:ml-10">
               <Link href="/">
                 <a>
                   <Logo />
@@ -33,22 +52,28 @@ export default function Navbar({ lastUpdated = 6 }) {
                 </a>
               </Link>
               <Link href="/team">
-                <a className="hover:bg-gray-700 text-gray hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                  Team
-                </a>
+                <a className="hover:bg-gray-700 text-gray hover:text-white px-3 py-2 rounded-md text-sm font-medium"></a>
               </Link>
             </div>
-            <div className="-mr-2 flex md:hidden">
-              {/* Mobile menu button */}
-              <Disclosure.Button className="bg-gray-300 inline-flex items-center justify-center p-2 rounded-md text-gray-800 focus:outline-none focus:ring-2 focus:ring-white">
-                <span className="sr-only">Open main menu</span>
-                {open ? (
-                  <XIcon className="block h-6 w-6" aria-hidden="true" />
-                ) : (
-                  <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                )}
-              </Disclosure.Button>
-            </div>
+            {canShare ? (
+              <button
+                className="block md:hidden"
+                onClick={async () => {
+                  try {
+                    await navigator.share({
+                      title: "Covid.army",
+                      text:
+                        "Verified Real Time List of COVID-19 Resources and Aid",
+                      url: "https://covid.army",
+                    })
+                  } catch (err) {}
+                }}
+              >
+                <HiOutlineShare className="block h-6 w-6" />
+              </button>
+            ) : (
+              <div className="h-1 w-1"></div>
+            )}
           </div>
           <Disclosure.Panel className="md:hidden">
             <div className="px-2 grid pt-2 pb-3 space-y-1 sm:px-3">
