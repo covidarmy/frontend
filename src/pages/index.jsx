00fea7a8927d2883a7240b7205d62d993eb77c41
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-04 10:42:19 +0530

    disclaimer added at the top

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,9 @@ const IndexPage = ({ resources, cities, lastUpdated }) => {
   return (
     <div className="w-screen h-screen flex flex-col">
       <Navbar lastUpdated={lastUpdated} />
+      <div className='mt-8 text-center'>
+        <p className='text-red-500 font-bold'> 🚨 Please beware of fraudsters 🚨</p>
+      </div>
       <Dashboard
         data={{
           resources,
