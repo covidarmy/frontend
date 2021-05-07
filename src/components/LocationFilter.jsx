da7a44dbbd2e9859f4a0a51df04ed8f5de997853
jai-sharma <sendtojsharma@gmail.com>
2021-05-07 08:55:50 +0530

    simplifying logic

--- src/components/LocationFilter.jsx
+++ src/components/LocationFilter.jsx
@@ -1,40 +1,43 @@
 import Fuse from "fuse.js"
 import { useRouter } from "next/router"
 import FilterButton from "./FilterButton"
-import { useEffect, useState } from "react"
+import * as React from "react"
 import LocationIcon from "../assets/Location.svg"
 import SearchIcon from "../assets/Search.svg"
 import { HiChevronDown as DownArrow } from "react-icons/hi"
 import { HiChevronUp as UpArrow } from "react-icons/hi"
 import { useCities } from "~/hooks/useCities"
 import Skeleton from "react-loading-skeleton"
+import { useSlug } from "~/context/slug"
 
-export default function LocationFilter({ city, resource }) {
-  const { data, error } = useCities()
-  const [resources, setResources] = useState(data)
-  const [cityState, setCityState] = useState(city ? true : false)
-
-  useEffect(() => {
-    if (data) {
-      setResources(Object.keys(data))
-    }
-  }, [data])
+export default function LocationFilter() {
+  const { location, resource } = useSlug()
+  const [cities, error, isLoading] = useCities()
+  const [cityState, setCityState] = React.useState(false)
 
   const router = useRouter()
   const filter = router.pathname === "/" && "all"
-  const [showMore, setShowMore] = useState(false)
+  const [showMore, setShowMore] = React.useState(false)
 
-  const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = React.useState("")
+
+  React.useEffect(() => {
+    if (typeof location !== "undefined") {
+      setCityState(true)
+    } else {
+      setCityState(false)
+    }
+  }, [location])
 
   if (error) return <div>failed to load</div>
-  if (!data) return <Skeleton height={214} />
+  if (isLoading) return <Skeleton height={214} />
 
   const renderButtons = () => {
     let _data = null
 
     if (searchValue) {
       const fuse = new Fuse(
-        resources.sort().filter((i) => typeof i !== "boolean"),
+        cities.sort().filter((i) => typeof i !== "boolean"),
         { includeScore: true }
       )
 
@@ -60,23 +63,29 @@ export default function LocationFilter({ city, resource }) {
         _data = [..._data, filter]
       }
     } else if (_data === null) {
-      _data = resources.sort()
+      _data = cities.sort()
     }
 
     _data = _data.filter((i) => typeof i !== "boolean")
 
     return _data.map((item) => {
+      /** Location provided by useSlug */
+      let currentLocation = location
+      /** Location where the FilterButton will route the user to */
+      const buttonLocation = item.replace(/\s+/g, "").toString().toLowerCase()
+
+      if (typeof currentLocation === "string") {
+        currentLocation = currentLocation.replace(/\s+/g, "").toLowerCase()
+      }
+
       return (
         <FilterButton
           key={item}
-          active={
-            typeof city === "string" &&
-            city.toLowerCase() === item.toLowerCase()
-          }
+          active={currentLocation === item.toLowerCase()}
           href={
-            resource === null
-              ? "/" + item.toString().toLowerCase()
-              : `/${item.toString().toLowerCase()}/${resource}`
+            resource === undefined
+              ? "/" + buttonLocation
+              : `/${buttonLocation}/${resource}`
           }
         >
           {item}
@@ -85,55 +94,54 @@ export default function LocationFilter({ city, resource }) {
     })
   }
 
-  return !cityState ? (
-    <div className="shadow-md bg-white box-border h-auto w-full rounded-md my-2 p-3 lg:p-6 border border-gray-200">
-      {/* ICON */}
-      <div className="flex ml-1 mb-1">
-        <LocationIcon className="h-5 w-5 mt-1" />
-        <p className="text-strong ml-1 mt-0.5 font-bold">Choose Your City</p>
+  if (!cityState)
+    return (
+      <div className="shadow-md bg-white box-border h-auto w-full rounded-md my-2 p-3 lg:p-6 border border-gray-200">
+        <div className="flex ml-1 mb-1">
+          <LocationIcon className="h-5 w-5 mt-1" />
+          <p className="text-strong ml-1 mt-0.5 font-bold">Choose Your City</p>
+        </div>
+        <div className="pt-2 ml-1 flex justify-start relative text-gray-600">
+          <input
+            className="border-2 w-full relative w-400 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            type="search"
+            name="search"
+            placeholder="Search for your city or Select from below"
+            onChange={({ currentTarget }) =>
+              setSearchValue(currentTarget.value)
+            }
+          />
+          <button type="submit" className="relative h-7 w-7 ml-1 mt-2">
+            <SearchIcon />
+          </button>
+        </div>
+        <div className="mt-2 text-start text-left flex-wrap flex items-center justify-start">
+          {renderButtons()}
+        </div>
+        <div className="mt-2 ml-1">
+          <button
+            className="hover:text-underline flex text-indigo-600"
+            onClick={() => setShowMore((prev) => !prev)}
+          >
+            <span>
+              {showMore ? "Show only top locations" : "Show all locations"}
+            </span>
+            {showMore && <UpArrow />}
+            {!showMore && <DownArrow />}
+          </button>
+        </div>
       </div>
-      {/* SEARCHBOX */}
-      <div className="pt-2 ml-1 flex justify-start relative text-gray-600">
-        <input
-          className="border-2 w-full relative w-400 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-          type="search"
-          name="search"
-          placeholder="Search for your city or Select from below"
-          onChange={({ currentTarget }) => setSearchValue(currentTarget.value)}
-        />
-        <button type="submit" className="relative mt-0">
-          <SearchIcon />
-        </button>
-      </div>
-      {/* RENDER PELLETS */}
-      <div className="mt-2 text-start text-left flex-wrap flex items-center justify-start">
-        {renderButtons()}
-      </div>
-      {/* SHOW MORE */}
-      <div className="mt-2 ml-1">
-        <button
-          className="hover:text-underline flex text-indigo-600"
-          onClick={() => setShowMore((prev) => !prev)}
-        >
-          <span>
-            {showMore ? "Show only top locations" : "Show all locations"}
-          </span>
-          {showMore && <UpArrow />}
-          {!showMore && <DownArrow />}
-        </button>
-      </div>
-    </div>
-  ) : (
+    )
+
+  return (
     <div className="shadow-md bg-white box-border h-auto w-full rounded-md my-2 p-3 lg:p-6 border border-gray-200">
       <div className="flex ml-1 justify-between">
         <div className="flex">
-          {/* ICON */}
           <LocationIcon />
-          {/* CITY NAME */}
-          <p className="text-strong ml-1 mt-0.5 font-bold capitalize">{city}</p>
+          <p className="text-strong ml-1 mt-0.5 font-bold capitalize">
+            {location}
+          </p>
         </div>
-
-        {/* CHANGE BUTTON */}
         <button onClick={() => setCityState(!cityState)}>
           <span className="font-bold text-primary">Change</span>
         </button>
--- src/components/ResourceFilter.jsx
+++ src/components/ResourceFilter.jsx
@@ -3,16 +3,17 @@ import ResourceIcon from "../assets/Resource.svg"
 import ResourceIconDeactivated from "../assets/ResourceDeactivated.svg"
 import { useResources } from "~/hooks/useResources"
 import Skeleton from "react-loading-skeleton"
+import { useSlug } from "~/context/slug"
 
-export default function ResourceFilter({ city, resource }) {
-  const { data, error } = useResources()
+export default function ResourceFilter() {
+  const { location, resource } = useSlug()
+  const [resources, error, isLoading] = useResources()
 
+  // we can add better error state later
   if (error) return <div>failed to load</div>
-  if (!data) return <Skeleton height={128} />
+  if (isLoading) return <Skeleton height={128} />
 
-  const resources = Object.keys(data)
-
-  if (city) {
+  if (location)
     return (
       <div className="shadow-md border border-gray-200 rounded-md bg-white text-center box-border h-auto w-full my-2 p-3 lg:p-6">
         <div className="flex">
@@ -21,17 +22,15 @@ export default function ResourceFilter({ city, resource }) {
         </div>
         <div className="mt-2 text-start text-left flex-wrap flex items-center justify-start">
           {resources.map((item) => {
+            const buttonResource = item.toLowerCase()
             return (
               <FilterButton
                 key={item}
-                active={
-                  typeof resource === "string" &&
-                  item.toLowerCase() === resource.toLowerCase()
-                }
+                active={resource === buttonResource}
                 href={
-                  city === null
-                    ? `/${item.toLowerCase()}`
-                    : `/${city}` + `/${item.toLowerCase()}`
+                  location === null
+                    ? `/${buttonResource}`
+                    : `/${location}` + `/${buttonResource}`
                 }
               >
                 {item}
@@ -41,16 +40,15 @@ export default function ResourceFilter({ city, resource }) {
         </div>
       </div>
     )
-  } else {
-    return (
-      <div className="shadow-md border border-gray-200 rounded-md bg-gray-100 text-center box-border h-auto w-full my-2 p-3 lg:p-6 cursor-not-allowed">
-        <div className="flex">
-          <ResourceIconDeactivated />
-          <p className="text-strong mt-0 ml-1 font-bold text-gray-500">
-            Please select city first
-          </p>
-        </div>
+
+  return (
+    <div className="shadow-md border border-gray-200 rounded-md bg-gray-100 text-center box-border h-auto w-full my-2 p-3 lg:p-6 cursor-not-allowed">
+      <div className="flex">
+        <ResourceIconDeactivated />
+        <p className="text-strong mt-0 ml-1 font-bold text-gray-500">
+          Please select city first
+        </p>
       </div>
-    )
-  }
+    </div>
+  )
 }
