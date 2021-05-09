20c4d5eb1fcc7ab664d70c82611795ce8d2b13d8
jai-sharma <sendtojsharma@gmail.com>
2021-05-09 19:01:43 +0530

    changes for the new city data

--- src/hooks/useCities.js
+++ src/hooks/useCities.js
@@ -1,16 +1,27 @@
-import { API_BASE_URL } from "~/lib/url"
 import fetcher from "~/lib/fetcher"
 import useSWR from "swr"
 import * as React from "react"
 
+const getCitiesFromData = (data) => {
+  const cities = []
+  data.map((item) => {
+    cities.push(item.name)
+  })
+
+  return cities
+}
+
 export const useCities = () => {
-  const { data, error } = useSWR(`${API_BASE_URL}/api/cities`, fetcher)
+  const { data, error } = useSWR(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/cities`,
+    fetcher
+  )
   const [cities, setCities] = React.useState(data)
   const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
     if (data) {
-      setCities(Object.keys(data))
+      setCities(getCitiesFromData(data))
       setIsLoading(false)
     }
   }, [data])
