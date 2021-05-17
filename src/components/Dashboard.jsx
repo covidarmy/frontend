d9dc82080b769896afda0f94e5dd9be92df3fb98
jai-sharma <sendtojsharma@gmail.com>
2021-05-17 21:15:15 +0530

    removing disclaimer from here, and adding floating whatsapp button

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 
+import WhatsappLogo from "../assets/whatsapp.svg"
+
 // COMPONENTS
 import LocationFilter from "./LocationFilter"
 import ResourceFilter from "./ResourceFilter"
@@ -9,27 +11,13 @@ import Footer from "./Footer"
 export const Dashboard = () => {
   return (
     <>
-      <div className="flex flex-col justify-between lg:overflow-hidden lg:h-full h-auto lg:mt-6">
-        <div className="mt-4 text-center mb-4 md:mb-0">
-          <p className="text-xs md:text-sm text-red-600 font-bold">
-            🚨 Beware of fraud. Sign{" "}
-            <a
-              href="https://www.change.org/p/government-of-india-should-approve-paper-strip-rapid-antigen-tests-for-self-testing-at-home"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline"
-            >
-              this
-            </a>{" "}
-            petition to end covid. 🚨
-          </p>
-        </div>
+      <div className="flex flex-col lg:overflow-hidden min-h-screen">
         <div className="flex flex-col justify-center px-4 overflow-hidden lg:flex-row lg:mt-6 h-full">
           <div className="rounded-md flex flex-col lg:w-1/2">
             <LocationFilter />
             <ResourceFilter />
           </div>
-          <div className="h-auto overflow-y-auto flex-col items-center lg:w-1/2 hidden lg:flex">
+          <div className="h-auto mx-8 flex-col items-center lg:w-1/2 hidden lg:flex">
             <TweetsList />
           </div>
           <div className="block lg:hidden">
@@ -38,6 +26,15 @@ export const Dashboard = () => {
         </div>
       </div>
       <Footer />
+      <div className="fixed bottom-6 right-6 w-10">
+        <a
+          href="http://wa.me/917404255034"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <WhatsappLogo />
+        </a>
+      </div>
     </>
   )
 }
