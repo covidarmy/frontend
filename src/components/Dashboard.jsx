de7cb4a37930851861179e716c46c683b6299d52
jai-sharma <sendtojsharma@gmail.com>
2021-05-18 10:59:54 +0530

    fixing link

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -28,7 +28,7 @@ export const Dashboard = () => {
       <Footer />
       <div className="shadow-2xl rounded-full p-2 bg-green-600 fixed bottom-6 right-6 w-12 cursor-pointer">
         <a
-          href="http://wa.me/917404255034"
+          href="https://wa.me/917404255034?text=Hi"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Whatsapp Bot"
