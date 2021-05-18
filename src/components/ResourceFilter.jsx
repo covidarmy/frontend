25cd3784162962dce181f7b93f358983eca66b74
jai-sharma <sendtojsharma@gmail.com>
2021-05-18 06:20:20 +0530

    adding banner

--- src/components/ResourceFilter.jsx
+++ src/components/ResourceFilter.jsx
@@ -5,6 +5,7 @@ import { useResources } from "~/hooks/useResources"
 import Skeleton from "react-loading-skeleton"
 import { useSlug } from "~/context/slug"
 import { useTranslation } from "~/context/translation"
+import FruadBanner from "./FruadBanner"
 
 export default function ResourceFilter() {
   const { location, resource } = useSlug()
@@ -19,10 +20,11 @@ export default function ResourceFilter() {
     return (
       <div className="shadow-md border border-gray-200 rounded-md bg-white text-center box-border h-auto w-full my-2 p-3 lg:p-6">
         <div className="flex items-center">
-          <ResourceIcon />
-          <p className="text-strong mt-0 ml-1 font-bold">
+          <ResourceIcon className="h-5 w-5" />
+          <p className="text-strong ml-1 font-bold text-sm md:text-base">
             {t("CHOOSE_RESOURCES")}
           </p>
+          <FruadBanner />
         </div>
         <div className="mt-2 text-start text-left flex-wrap flex items-center justify-start">
           {resources.map((item) => {
