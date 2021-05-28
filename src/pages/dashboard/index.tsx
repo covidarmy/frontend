85db6c64785509acacf9332baf713643d93f8774
jai-sharma <sendtojsharma@gmail.com>
2021-05-28 19:13:26 +0530

    adding index page back

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,43 +1,41 @@
 import * as React from "react"
+import { useRouter } from "next/router"
+import AddLeadButton from "~/components/AddLeadButton"
+import LoadingPage from "~/components/LoadingPage"
 import Navbar from "~/components/Navbar"
 import { useAuth } from "~/context/auth"
-import { useRouter } from "next/router"
-import LoadingPage from "~/components/LoadingPage"
-import { useStore } from "~/lib/StepsStore"
 
-import StatesStep from "~/components/Steps/States"
-import CitiesStep from "~/components/Steps/Cities"
-import ResourceStep from "~/components/Steps/Resources"
-import SubmitForm from "~/components/Steps/SubmitForm"
-
-export default function AddResourcePage() {
-  const { nextStep, previousStep, step } = useStore((state) => ({
-    step: state.step,
-    previousStep: state.actions.previousStep,
-    nextStep: state.actions.nextStep,
-  }))
-
+export default function DashboardPage() {
+  const { isAuthenticated, loading } = useAuth()
   const router = useRouter()
-  const { user, isAuthenticated, loading } = useAuth()
+  const leads = []
 
   React.useEffect(() => {
     if (!loading && !isAuthenticated) {
       router.push("/login")
     }
-  }, [loading, isAuthenticated])
+  }, [isAuthenticated, loading])
 
   if (loading) return <LoadingPage />
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-gray-100 ">
       <Navbar />
-      {step === 1 && <StatesStep nextStep={nextStep} />}
-      {step === 2 && (
-        <CitiesStep nextStep={nextStep} previousStep={previousStep} />
-      )}
-      {step === 3 && (
-        <ResourceStep nextStep={nextStep} previousStep={previousStep} />
-      )}
-      {step === 4 && <SubmitForm previousStep={previousStep} user={user} />}
+      <main className="flex flex-col items-center justify-center p-4 pt-8 mx-auto rounded-lg lg:pt-12 md:pt-8 lg:px-0 lg:w-2/5">
+        <div className="flex items-center justify-between w-full">
+          <h1 className="mx-auto text-xl font-bold">Submitted leads</h1>
+          {leads.length > 0 && <AddLeadButton />}
+        </div>
+        <div className="flex flex-col gap-12 mt-16">
+          {leads.length === 0 ? (
+            <div className="flex flex-col items-center justify-center gap-6">
+              <span className="text-lg font-medium">No leads found.</span>
+              <AddLeadButton />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </main>
     </div>
   )
 }
