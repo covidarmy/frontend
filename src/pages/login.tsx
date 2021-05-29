5de05ff49e159f5639f5b768fe6e835b5327f895
jai-sharma <sendtojsharma@gmail.com>
2021-05-29 13:17:27 +0530

    switiching to OAuth logins

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,16 +7,13 @@ import RightIcon from "~/assets/chevron-right.svg"
 import { APP_BASE_URL } from "~/constants"
 import { useAuth } from "~/context/auth"
 import LoadingPage from "~/components/LoadingPage"
-import { auth } from "~/lib/firebase"
+import { auth, fb } from "~/lib/firebase"
+import FaceBookIcon from "~/assets/facebook.svg"
+import GoogleIcon from "~/assets/google.svg"
 
 const LoginPage: React.FC = () => {
   const { isAuthenticated, loading } = useAuth()
   const router = useRouter()
-  const [email, setEmail] = React.useState<string>("")
-  const [submitted, setSubmitted] = React.useState({
-    done: false,
-    email: "",
-  })
 
   React.useEffect(() => {
     if (!loading && isAuthenticated) {
@@ -29,7 +26,7 @@ const LoginPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <main className="flex flex-col items-center justify-center rounded-lg p-4 sm:p-8 lg:w-[40rem] gap-5 w-4/5 mx-auto">
+      <main className="flex flex-col items-center justify-center rounded-lg p-4 sm:p-8 gap-5 mx-auto max-w-xl">
         <div className="w-full px-6 py-6 bg-white rounded-md shadow-md sm:px-10">
           <div className="flex items-center">
             <Link href="/">
@@ -42,60 +39,43 @@ const LoginPage: React.FC = () => {
             </div>
           </div>
           <hr className="mt-6" />
-          <div className="mt-6">
-            <h2 className="text-xl font-bold">
-              Please enter your email and press submit.
-            </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              You will be sent a login link to your email on submission.
-            </p>
-          </div>
-          {!submitted.done && (
-            <form
-              className="flex flex-col items-center justify-between w-full mt-6"
-              onSubmit={async (e) => {
-                e.preventDefault()
-                // await fetch(API_BASE_URL + "/volunteer/login", {
-                //   method: "POST",
-                //   body: JSON.stringify({ email }),
-                // })
-                console.log(email)
-                await auth.sendSignInLinkToEmail(email, {
-                  url: APP_BASE_URL + "/auth/success",
-                  handleCodeInApp: true,
-                })
-                window.localStorage.setItem("emailForSignIn", email)
-                setSubmitted({ done: true, email })
+          <p className="mt-7 text-gray-400">
+            Hello Superhero, hope you are doing good!
+          </p>
+          <h2 className="text-xl font-bold mt-8">Please log in using</h2>
+          <div className="flex justify-between items-center mt-9">
+            <button
+              className="inline-flex items-center justify-center py-3 px-7 rounded-lg"
+              style={{ background: "#EFEFEF" }}
+              onClick={() => {
+                auth
+                  .signInWithPopup(new fb.auth.GoogleAuthProvider())
+                  .then((user) => {
+                    router.push("/dashboard")
+                  })
               }}
             >
-              <div className="flex items-center justify-start w-full gap-8">
-                <label className="sr-only">Email</label>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full px-2 py-2 transition ease-in border border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-offset-indigo-400"
-                  required
-                />
-              </div>
-              <div className="flex w-full mt-6">
-                <button
-                  type="submit"
-                  className="w-32 py-2 text-sm font-medium text-white transition ease-in bg-indigo-500 rounded focus:outline-none focus:ring focus:ring-offset-indigo-400"
-                >
-                  Send login link
-                </button>
-              </div>
-            </form>
-          )}
-
-          {submitted.done && (
-            <div className="flex items-center justify-start w-full px-4 py-2 mt-6 text-sm text-green-800 bg-green-200 rounded">
-              Sent login link to {submitted.email}. Refresh and submit form
-              again if you haven't received an email.
-            </div>
-          )}
+              <GoogleIcon />
+              <div className="text-xl font-semibold ml-2">Google</div>
+            </button>
+            <p>or</p>
+            <button
+              className="inline-flex items-center justify-center py-3 px-7 rounded-lg"
+              style={{ background: "#EFEFEF" }}
+              onClick={() => {
+                auth
+                  .signInWithPopup(new fb.auth.FacebookAuthProvider())
+                  .then((user) => {
+                    router.push("/dashboard")
+                  })
+              }}
+            >
+              <FaceBookIcon />
+              <div className="text-xl font-semibold ml-2">Facebook</div>
+            </button>
+          </div>
         </div>
+
         <div className="w-full px-6 py-6 bg-white rounded-lg shadow-md sm:px-10">
           <div className="flex items-center justify-between text-blue-500 cursor-pointer hover:underline">
             <div>Volunteer FAQs</div>
