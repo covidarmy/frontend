923c49a01b96a98da34e735fe36b0ed41b9ed7bf
jai-sharma <sendtojsharma@gmail.com>
2021-05-31 12:37:01 +0530

    improving filter button

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,6 +1,6 @@
-import clsx from "clsx";
-import * as React from "react";
-import Link from "next/link";
+import clsx from 'clsx';
+import * as React from 'react';
+import Link from 'next/link';
 
 /**
  * @typedef {Object} Props
@@ -15,29 +15,25 @@ import Link from "next/link";
  * @type {React.ForwardRefExoticComponent<Props>}
  */
 const FilterButton = React.forwardRef(
-  (
-    { active, href = "", onClick, children, className = "", isButton = false },
-    ref
-  ) => {
-    const classes = clsx([
-      "hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-0.5 px-1 text-sm lg:text-base lg:py-1.5 m-1 lg:px-2 border-transparent hover:border-gray-500 rounded select-none cursor-pointer transition-colors",
-      active ? "bg-gray-500 text-white" : "bg-gray-200",
-      className,
-    ]);
+    ({ active, href = '', onClick, children, isButton = false }, ref) => {
 
-    switch (true) {
-      case isButton:
-        return <button className={classes} onClick={onClick}></button>;
-      default:
-        return (
-          <Link href={href}>
-            <a ref={ref} onClick={onClick} className={classes}>
-              {children}
-            </a>
-          </Link>
-        );
+        const classes = `px-3 py-2 mx-2 my-1 rounded transition-colors ${
+            active ? 'bg-blue-600 text-white' : 'bg-gray-200 hover:bg-gray-500 hover:text-white'
+        }`;
+
+        switch (true) {
+            case isButton:
+                return <button className={classes} onClick={onClick}></button>;
+            default:
+                return (
+                    <Link href={href}>
+                        <a ref={ref} onClick={onClick} className={classes}>
+                            {children}
+                        </a>
+                    </Link>
+                );
+        }
     }
-  }
 );
 
 export default FilterButton;
