e642617fa1af1a593ed3064aa94d8e84e9371be4
jai-sharma <sendtojsharma@gmail.com>
2021-05-31 14:21:38 +0530

    improving layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,38 +1,38 @@
-import * as React from "react";
-import WhatsappLogo from "../assets/whatsapp.svg";
-import LocationFilter from "./LocationFilter";
-import ResourceFilter from "./ResourceFilter";
-import TweetsList from "./TweetsList";
-import Footer from "./Footer";
+import * as React from 'react';
+import WhatsappLogo from '../assets/whatsapp.svg';
+import LocationFilter from './LocationFilter';
+import ResourceFilter from './ResourceFilter';
+import TweetsList from './TweetsList';
+import Footer from './Footer';
+import { isDesktop, isMobile, isTablet } from 'react-device-detect';
 
 export const Dashboard = () => {
-  return (
-    <>
-      <main className="flex flex-col lg:overflow-hidden min-h-screen">
-        <div className="flex flex-col justify-center px-4 overflow-hidden lg:flex-row lg:mt-6 h-full">
-          <div className="rounded-md flex flex-col lg:w-1/2">
-            <LocationFilter />
-            <ResourceFilter />
-          </div>
-          <div className="h-auto mx-8 flex-col items-center lg:w-1/2 hidden lg:flex">
-            <TweetsList />
-          </div>
-          <div className="block lg:hidden">
-            <TweetsList />
-          </div>
-        </div>
-      </main>
-      <Footer />
-      <div className="shadow-2xl rounded-full p-2 bg-green-600 fixed bottom-4 right-6 w-12 cursor-pointer">
-        <a
-          href="https://wa.me/917404255034?text=Hi"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Whatsapp Bot"
-        >
-          <WhatsappLogo />
-        </a>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <main
+                className="container mx-auto p-2 grid md:grid-cols-2 md:gap-8 lg:mt-4"
+                style={{ maxWidth: '100vw' }}
+            >
+                <div className="flex flex-col">
+                    <LocationFilter />
+                    <ResourceFilter />
+                    {isDesktop && <Footer />}
+                </div>
+                <div>
+                    <TweetsList />
+                    <div className="shadow-2xl rounded-full p-2 bg-green-600 fixed bottom-3 right-6 w-12 cursor-pointer">
+                        <a
+                            href="https://wa.me/917404255034?text=Hi"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Whatsapp Bot"
+                        >
+                            <WhatsappLogo />
+                        </a>
+                    </div>
+                    {(isMobile || isTablet) && <Footer />}
+                </div>
+            </main>
+        </>
+    );
 };
