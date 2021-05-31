d787ff4a875ad92daf2ae73279def63d8e20740f
jai-sharma <sendtojsharma@gmail.com>
2021-05-31 15:15:32 +0530

    more changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,35 +1,40 @@
-import * as React from "react";
-import WhatsappLogo from "../assets/whatsapp.svg";
-import LocationFilter from "./LocationFilter";
-import ResourceFilter from "./ResourceFilter";
-import TweetsList from "./TweetsList";
-import Footer from "./Footer";
-import { isDesktop, isMobile, isTablet } from "react-device-detect";
+import * as React from 'react';
+import WhatsappLogo from '../assets/whatsapp.svg';
+import LocationFilter from './LocationFilter';
+import ResourceFilter from './ResourceFilter';
+import TweetsList from './TweetsList';
+import Footer from './Footer';
+import { isDesktop, isMobile, isTablet } from 'react-device-detect';
+
+const FAB = () => {
+    return (
+        <div className="shadow-2xl rounded-full p-2 bg-green-600 fixed bottom-3 right-6 w-12 cursor-pointer" style={{ zIndex: '-1' }}>
+            <a
+                href="https://wa.me/917404255034?text=Hi"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Whatsapp Bot"
+            >
+                <WhatsappLogo />
+            </a>
+        </div>
+    );
+};
 
 export const Dashboard = () => {
-  return (
-    <>
-      <main className="container mx-auto p-2 grid md:grid-cols-2 md:gap-8 lg:mt-4">
-        <div className="flex flex-col">
-          <LocationFilter />
-          <ResourceFilter />
-          {isDesktop && <Footer />}
-        </div>
-        <div>
-          <TweetsList />
-          <div className="shadow-2xl rounded-full p-2 bg-green-600 fixed bottom-3 right-6 w-12 cursor-pointer">
-            <a
-              href="https://wa.me/917404255034?text=Hi"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Whatsapp Bot"
-            >
-              <WhatsappLogo />
-            </a>
-          </div>
-          {(isMobile || isTablet) && <Footer />}
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="container mx-auto p-2 grid md:grid-cols-2 md:gap-8 lg:mt-4">
+                <div className="flex flex-col">
+                    <LocationFilter />
+                    <ResourceFilter />
+                    {isDesktop && <Footer />}
+                </div>
+                <div className="flex items-center justify-center">
+                    <TweetsList />
+                </div>
+            </main>
+            {/* {(isMobile || isTablet) && <FAB />} */}
+        </>
+    );
 };
--- src/components/TweetsList.jsx
+++ src/components/TweetsList.jsx
@@ -1,112 +1,119 @@
-import { Tweet } from "react-static-tweets";
-import { HiChevronDoubleDown } from "react-icons/hi";
-import { useTweets } from "~/hooks/useTweets";
-import Skeleton from "react-loading-skeleton";
-import { useSlug } from "~/context/slug";
-import { useTranslation } from "~/context/translation";
+import { Tweet } from 'react-static-tweets';
+import { HiChevronDoubleDown } from 'react-icons/hi';
+import { useTweets } from '~/hooks/useTweets';
+import Skeleton from 'react-loading-skeleton';
+import { useSlug } from '~/context/slug';
+import { useTranslation } from '~/context/translation';
+import Footer from './Footer';
+import { isMobile, isTablet } from 'react-device-detect';
 
 const OlaNotice = () => {
-  return (
-    <div className="w-full flex flex-col items-center justify-center space-y-4 my-2 px-2">
-      <a
-        className="p-4 rounded-lg text-center border border-gray-200 cursor-pointer"
-        href="https://www.olacabs.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        You can request a oxygen concentrator from your Ola app for no charge!
-      </a>
-    </div>
-  );
+    return (
+        <div className="w-full flex flex-col items-center justify-center space-y-4 my-2 px-2">
+            <a
+                className="p-4 rounded-lg text-center border border-gray-200 cursor-pointer"
+                href="https://www.olacabs.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                You can request a oxygen concentrator from your Ola app for no
+                charge!
+            </a>
+        </div>
+    );
 };
 
 const TweetsList = () => {
-  const { location, resource } = useSlug();
-  const { data, error, size, setSize } = useTweets({ location, resource });
-  const { t } = useTranslation();
+    const { location, resource } = useSlug();
+    const { data, error, size, setSize } = useTweets({ location, resource });
+    const { t } = useTranslation();
 
-  const isOxygenConcentratorFromBanglore =
-    location === "bangalore" && resource === "oxygenconcentrator";
+    const isOxygenConcentratorFromBanglore =
+        location === 'bangalore' && resource === 'oxygenconcentrator';
 
-  if (error) return <div>failed to load</div>;
-  if (!data)
+    if (error) return <div>failed to load</div>;
+    if (!data)
+        return (
+            <div className="space-y-4 px-0 lg:px-12">
+                <Skeleton count={4} height={340} />
+            </div>
+        );
+
+    const showMore = () => {
+        setSize(size + 1);
+    };
+
+    const isEmpty = data?.[0]?.length === 0;
+    const isReachingEnd =
+        isEmpty || (data && data[data.length - 1]?.length < 20);
+
+    if (!location) {
+        return (
+            <div className="hidden lg:flex justify-center items-center border rounded-md shadow-md bg-gray-100 w-full h-52 my-2 p-3 lg:p-6">
+                <p className="font-semibold text-lg text-gray-600">
+                    {t('SELECT_LOCATION_RESOURCE')}
+                </p>
+            </div>
+        );
+    }
+
+    if (!resource) {
+        return (
+            <div className="hidden lg:flex justify-center items-center border rounded-md shadow-md bg-gray-100 w-full h-52 my-2 p-3 lg:p-6">
+                <p className="font-semibold text-lg text-gray-600">
+                    {t('SELECT_RESOURCE')}
+                </p>
+            </div>
+        );
+    }
+
+    if (data[0].length > 0) {
+        return (
+            <div className="flex flex-col">
+                <div className="flex flex-col items-center justify-center">
+                    {isOxygenConcentratorFromBanglore ? <OlaNotice /> : ''}
+                    {
+                        // Tweets
+                        data.map((page) => {
+                            return page.map(({ _id: key, tweet_id }) => (
+                                <div
+                                    key={key}
+                                    className="flex flex-col items-center justify-center space-y-4 my-2 w-full max-w-full"
+                                >
+                                    <Tweet id={tweet_id} />
+                                </div>
+                            ));
+                        })
+                    }
+                    {!isReachingEnd && (
+                        <button
+                            onClick={showMore}
+                            style={{ maxWidth: '550px' }}
+                            className="bg-indigo-200 text-indigo-700 w-full flex items-center justify-center p-2 mt-2 mb-4 rounded-lg gap-1 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            disabled={data[0].length % 20 !== 0}
+                        >
+                            Load more
+                            <HiChevronDoubleDown className="mt-0.5" />
+                        </button>
+                    )}
+                </div>
+                {(isMobile || isTablet) && <Footer />}
+            </div>
+        );
+    }
+
     return (
-      <div className="space-y-4 px-0 lg:px-12">
-        <Skeleton count={4} height={340} />
-      </div>
+        // <div className="text-center pt-2 md:px-10">
+        //   {/* Sorry, we don't have any data{" "}
+        //   {location ? " for " + location + (resource ? " & " + resource : "") : ""}. */}
+        // </div>
+
+        <div className="flex justify-center items-center border rounded-md shadow-md bg-gray-100 w-full h-52 my-2 p-3 lg:p-6">
+            <p className="font-semibold text-sm md:text-lg text-gray-600 text-center">
+                Sorry, we don't have any data for {location} & {resource}.
+            </p>
+        </div>
     );
-
-  const showMore = () => {
-    setSize(size + 1);
-  };
-
-  const isEmpty = data?.[0]?.length === 0;
-  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 20);
-
-  if (!location) {
-    return (
-      <div className="hidden lg:flex justify-center items-center border rounded-md shadow-md bg-gray-100 w-full h-52 my-2 p-3 lg:p-6">
-        <p className="font-semibold text-lg text-gray-600">
-          {t("SELECT_LOCATION_RESOURCE")}
-        </p>
-      </div>
-    );
-  }
-
-  if (!resource) {
-    return (
-      <div className="hidden lg:flex justify-center items-center border rounded-md shadow-md bg-gray-100 w-full h-52 my-2 p-3 lg:p-6">
-        <p className="font-semibold text-lg text-gray-600">
-          {t("SELECT_RESOURCE")}
-        </p>
-      </div>
-    );
-  }
-
-  if (data[0].length > 0) {
-    return (
-      <div className="flex flex-col items-center justify-center pb-16 md:pb-0">
-        {isOxygenConcentratorFromBanglore ? <OlaNotice /> : ""}
-        {
-          // Tweets
-          data.map((page) => {
-            return page.map(({ _id: key, tweet_id }) => (
-              <div
-                key={key}
-                className="w-full max-w-lg flex flex-col items-center justify-center space-y-4 my-2"
-              >
-                <Tweet id={tweet_id} />
-              </div>
-            ));
-          })
-        }
-        {!isReachingEnd && (
-          <button
-            onClick={showMore}
-            style={{ maxWidth: "550px" }}
-            className="bg-indigo-200 text-indigo-700 w-full flex items-center justify-center p-2 mt-2 mb-4 rounded-lg gap-1 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            disabled={data[0].length % 20 !== 0}
-          >
-            Load more
-            <HiChevronDoubleDown className="mt-0.5" />
-          </button>
-        )}
-      </div>
-    );
-  }
-
-  return (
-    // <div className="text-center pt-2 md:px-10">
-    //   {/* Sorry, we don't have any data{" "}
-    //   {location ? " for " + location + (resource ? " & " + resource : "") : ""}. */}
-    // </div>
-
-    <div className="flex justify-center items-center border rounded-md shadow-md bg-gray-100 w-full h-52 my-2 p-3 lg:p-6">
-      <p className="font-semibold text-sm md:text-lg text-gray-600 text-center">
-        Sorry, we don't have any data for {location} & {resource}.
-      </p>
-    </div>
-  );
 };
 
 export default TweetsList;
