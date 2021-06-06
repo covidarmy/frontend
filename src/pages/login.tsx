99e8ece6fcd8fb624dc396d6db657f5078cb0f9c
jai-sharma <sendtojsharma@gmail.com>
2021-06-06 09:37:00 +0530

    adding nudge

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,7 @@ import Navbar from '~/components/Navbar'
 import BackIcon from '~/assets/arrow-left.svg'
 import LoadingPage from '~/components/LoadingPage'
 import GoogleIcon from '~/assets/google.svg'
+import RightIcon from '~/assets/chevron-right.svg'
 
 import { useRouter } from 'next/router'
 import { useAuth } from '~/context/auth'
@@ -24,11 +25,8 @@ const LoginPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <main
-        className="flex flex-col items-center justify-center rounded-lg p-4 sm:p-8 gap-5 mx-auto"
-        style={{ maxWidth: '36rem' }}
-      >
-        <div className="w-full px-6 py-6 bg-white rounded-md shadow-md sm:px-10">
+      <main className="flex flex-col items-center justify-center rounded-lg mt-5 sm:p-8 gap-5 mx-auto max-w-xl">
+        <div className="w-full px-6 py-6 bg-white sm:rounded-md shadow-md sm:px-10">
           <div className="flex items-center">
             <Link href="/">
               <a aria-label="Back Button">
@@ -58,13 +56,20 @@ const LoginPage: React.FC = () => {
             <GoogleIcon />
             <div className="text-xl font-semibold ml-2">Google</div>
           </button>
-        </div>
 
-        {/* <div className="w-full px-6 py-6 bg-white rounded-lg shadow-md sm:px-10">
-                <div className="flex items-center justify-between text-blue-500 cursor-pointer hover:underline">
-                <div>Volunteer FAQs</div>
-                <RightIcon />
-            </div>
+          <div className="mt-12 bg-gray-100 border border-gray-200 rounded-lg p-3">
+            <p className="font-semibold">
+              By signing in, I agree that I will use covid.army only to help
+              myself and others by providing information true to my knowledge.
+            </p>
+          </div>
+        </div>
+        {/* 
+        <div className="w-full px-6 py-6 bg-white rounded-lg shadow-md sm:px-10">
+          <div className="flex items-center justify-between text-blue-500 cursor-pointer hover:underline">
+            <div>Volunteer FAQs</div>
+            <RightIcon />
+          </div>
         </div> */}
       </main>
     </div>
